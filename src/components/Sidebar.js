cff5e01dc85f1933baa872f179a0dc5d9a4b4dd1
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-01 15:30:40 +0200

    new

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,8 @@ function Sidebar() {
     return ( 
       <div className="sideBar">
         <div className="navList">
-          <div className="naviListItem navItem">Δ</div>
-          <div className="naviListItem navItem">Δ</div>
+          <div className="naviListItem navItem" data-tool-tip="test">Δ</div>
+          <div className="naviListItem navItem" data-tool-tip="wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww">Δ</div>
         </div>
       </div>
      );
