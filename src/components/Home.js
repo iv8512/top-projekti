f9a11faf4287b9b5506dbe87e6bf85140df7593f
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-01 18:30:06 +0200

    Pages fixed
    Pages longer than 100vh no longer stretch the page horizontally

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 function Home() {
     return ( 
-        <div className="page">
-            <h1>Olet Kotisivulla</h1>
+        <div className="page" id="HomePage">
+            <div className="header">
+                <h1>Olet Kotisivulla</h1>
+            </div>
         </div>
      );
 }
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,18 @@
 function Test() {
     return ( 
-        <div className="page">
-            <h1>Olet Testisivulla</h1>
+        <div className="page" id="TestiSivu">
+            <div className="header">
+                <h1>Olet Testisivulla</h1>
+            </div>
+            <div className="content" id ="TestiContent">
+                <h2>Finally! Some content!</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maiores aliquid beatae animi sunt repellat a saepe maxime fugiat quaerat ducimus et sed expedita quam reprehenderit provident, adipisci corporis? Esse nihil modi maxime, ut molestias harum et culpa eaque quod, velit blanditiis impedit fuga sint? Sit in quam ex officia, incidunt aspernatur voluptatibus cumque vero unde eius deserunt beatae sint alias fugit dolorem odit dolor, illum error adipisci, accusamus possimus maiores ipsa ut illo. Quia, ipsum! Asperiores numquam sint dolor! Aliquid maxime ullam voluptate soluta sunt numquam ipsam rem, distinctio recusandae eligendi similique vero, vitae eaque ea repellat porro praesentium!</p>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta vel autem dignissimos nihil. Perferendis ex asperiores quam maiores iste minus, consectetur veniam laudantium molestiae vero facilis consequatur, facere quis inventore? Aliquam beatae autem, saepe qui minima perferendis sapiente fuga molestias optio culpa soluta eaque rem corporis quisquam libero repellendus excepturi itaque quae. Totam labore sint itaque! Dolorem molestias vero aperiam deserunt odit quasi facilis modi nulla soluta dicta tempora, provident facere dolor quis maiores obcaecati, labore ipsum dignissimos enim ipsa! Quo accusantium ex unde nisi voluptatem neque autem quam ut sapiente quod odit impedit illo ducimus, velit inventore accusamus. Fuga?</p>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt delectus harum aspernatur cumque ipsum sint dolor, totam accusamus voluptatibus natus non laborum. Dicta, libero provident ullam vero at corrupti? Repellendus delectus sequi sapiente accusantium natus reprehenderit explicabo quae officia perferendis consectetur deserunt modi hic unde impedit asperiores incidunt, deleniti enim, rem illum nam. Delectus, consectetur magnam beatae quae a ut voluptatum labore autem ratione fuga, dolor dolore corporis cumque, debitis nam fugiat? Maxime commodi odio adipisci velit asperiores vitae architecto recusandae hic suscipit eius nisi amet repudiandae quas, molestiae alias neque qui modi atque soluta voluptatem? Accusantium, debitis enim!</p>
+                
+            </div>
         </div>
      );
 }
