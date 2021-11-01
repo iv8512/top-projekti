cf280c162d7ef0a06c797e121a1b13129770743a
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-01 14:17:39 +0200

    added switch to router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import {
   Route
 } from "react-router-dom";
 import Sidebar from './components/Sidebar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Sidebar/>
+        <Switch>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return ( 
+        <h1>TEST</h1>
+     );
+}
+
+export default Home;
