23a64eda18f5030099df102c6e25e7894dbc578e
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-01 15:28:09 +0200

    Sidebar rework + icon + icon extendo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,23 +2,12 @@ import { Link } from 'react-router-dom'
 
 function Sidebar() {
     return ( 
-        <header className="sidebar">
-            <input type="checkbox" id="nav-toggle" class="nav-toggle"></input>
-            <nav>
-            <div className="navheader">logo space</div>
-            <span className="navitem">navitem</span>
-            <div className="navlist">
-            <span className="navlist-item navitem">navlist-item</span>
-            <Link to="/Home" className="navlist-item navitem">Home</Link>
-            <Link to="/Test" className="navlist-item navitem">Test</Link>
-            <a href="#" className="navlist-item navitem">Link3</a>
-            <a href="#" className="navlist-item navitem">Link4</a>
-          </div>
-        </nav>
-        <label for="nav-toggle" className="nav-toggle-label">
-            <span></span>
-        </label>
-      </header>
+      <div className="sideBar">
+        <div className="navList">
+          <div className="naviListItem navItem">Δ</div>
+          <div className="naviListItem navItem">Δ</div>
+        </div>
+      </div>
      );
 }
 
