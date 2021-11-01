bc3903878e746b5db39a8afd667d7d56dd6404f7
iv8512 <82021077+iv8512@users.noreply.github.com>
2021-11-01 14:09:51 +0200

    Sidebar test

--- src/App.js
+++ src/App.js
@@ -3,35 +3,14 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-
-      <header className="sidebar">
-        <input type="checkbox" id="nav-toggle" class="nav-toggle"></input>
-        <nav>
-          <div className="navheader">logo space</div>
-          <span className="navitem">navitem</span>
-          <div className="navlist">
-            <span className="navlist-item navitem">navlist-item</span>
-            <a href="#" className="navlist-item navitem">Link1</a>
-            <a href="#" className="navlist-item navitem">Link2</a>
-            <a href="#" className="navlist-item navitem">Link3</a>
-            <a href="#" className="navlist-item navitem">Link4</a>
-          </div>
-        </nav>
-        <label for="nav-toggle" className="nav-toggle-label">
-          <span></span>
-        </label>
-      </header>
-
-      <div className="page">
-        <h2>Page content here</h2>
-      </div>
-
+      <Router>
+        <Sidebar/>
+      </Router>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
 function Sidebar() {
-    return (  );
+    return ( 
+        <header className="sidebar">
+            <input type="checkbox" id="nav-toggle" class="nav-toggle"></input>
+            <nav>
+            <div className="navheader">logo space</div>
+            <span className="navitem">navitem</span>
+            <div className="navlist">
+            <span className="navlist-item navitem">navlist-item</span>
+            <a href="#" className="navlist-item navitem">Link1</a>
+            <a href="#" className="navlist-item navitem">Link2</a>
+            <a href="#" className="navlist-item navitem">Link3</a>
+            <a href="#" className="navlist-item navitem">Link4</a>
+          </div>
+        </nav>
+        <label for="nav-toggle" className="nav-toggle-label">
+            <span></span>
+        </label>
+      </header>
+     );
 }
 
 export default Sidebar;
