f5ff9485fe2b5b58eff02fccab8a7aa2d8a3b8b2
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-02 08:56:33 +0200

    deleted test quote

--- src/components/Gantt.js
+++ src/components/Gantt.js
@@ -1,4 +1,4 @@
-import data from "../Data/ganntchart.json"
+import data from "../data/ganntchart.json"
 import Chartrow from "./Chartrow";
 
 function Gantt() {
@@ -9,7 +9,7 @@ function Gantt() {
             </div>
             <div className="content">
                 <h2>Gantt Chart</h2>
-                <p>Gantt Charts show the flow of work <q>test</q> during developement.</p>
+                <p>Gantt Charts show the flow of work during developement.</p>
                 {data.map((chartData, index) => (
                     <Chartrow key={index} vnum={chartData.viikko.viikkonum} />
                 ))}
