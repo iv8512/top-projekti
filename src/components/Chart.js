ddb7461c3d0f5c7aae97edc3cd4e6df2fe214e74
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-08 01:20:03 +0200

    Chart Update

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,6 +1,6 @@
 function Chart(props) {
 
-    var widthMultiplier = 1
+    var widthMultiplier = 2
 
     return ( 
         <>
@@ -11,6 +11,26 @@ function Chart(props) {
         <div className="row">
             <span className="label">{props.data.rows[1].label}</span>
             <span className="chartContent" style={{ marginLeft: props.data.rows[1].start * widthMultiplier + 'em', width: props.data.rows[1].end * widthMultiplier - props.data.rows[1].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[1].progress + '% , var(--color-dark1) ' + props.data.rows[1].progress + '%' }}></span>
+        </div>
+        <div className="row">
+            <span className="label">{props.data.rows[2].label}</span>
+            <span className="chartContent" style={{ marginLeft: props.data.rows[2].start * widthMultiplier + 'em', width: props.data.rows[2].end * widthMultiplier - props.data.rows[2].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[2].progress + '% , var(--color-dark1) ' + props.data.rows[2].progress + '%' }}></span>
+        </div>
+        <div className="row">
+            <span className="label">{props.data.rows[3].label}</span>
+            <span className="chartContent" style={{ marginLeft: props.data.rows[3].start * widthMultiplier + 'em', width: props.data.rows[3].end * widthMultiplier - props.data.rows[3].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[3].progress + '% , var(--color-dark1) ' + props.data.rows[3].progress + '%' }}></span>
+        </div>
+        <div className="row">
+            <span className="label">{props.data.rows[4].label}</span>
+            <span className="chartContent" style={{ marginLeft: props.data.rows[4].start * widthMultiplier + 'em', width: props.data.rows[4].end * widthMultiplier - props.data.rows[4].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[4].progress + '% , var(--color-dark1) ' + props.data.rows[4].progress + '%' }}></span>
+        </div>
+        <div className="row">
+            <span className="label">{props.data.rows[5].label}</span>
+            <span className="chartContent" style={{ marginLeft: props.data.rows[5].start * widthMultiplier + 'em', width: props.data.rows[5].end * widthMultiplier - props.data.rows[5].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[5].progress + '% , var(--color-dark1) ' + props.data.rows[5].progress + '%' }}></span>
+        </div>
+        <div className="row">
+            <span className="label">{props.data.rows[6].label}</span>
+            <span className="chartContent" style={{ marginLeft: props.data.rows[6].start * widthMultiplier + 'em', width: props.data.rows[6].end * widthMultiplier - props.data.rows[6].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[6].progress + '% , var(--color-dark1) ' + props.data.rows[6].progress + '%' }}></span>
         </div>
         </>
      );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,9 @@ function Sidebar() {
     return ( 
       <div className="sideBar">
         <div className="navList">
-          <Link to="/"><div className="naviListItem navItem" data-tool-tip="Linkki Kotisivulle">Δ</div></Link>
-          <Link to="/test"><div className="naviListItem navItem" data-tool-tip="testi sivu">Δ</div></Link>
-          <Link to="/ganttchart"><div className="naviListItem navItem" data-tool-tip="Gantt Chart Visualised">Δ</div></Link>
+          <Link to="/" className="tabFocus"><div className="naviListItem navItem" data-tool-tip="Linkki Kotisivulle">Δ</div></Link>
+          <Link to="/test" className="tabFocus"><div className="naviListItem navItem" data-tool-tip="testi sivu">Δ</div></Link>
+          <Link to="/ganttchart" className="tabFocus"><div className="naviListItem navItem" data-tool-tip="Gantt Chart Visualised">Δ</div></Link>
         </div>
       </div>
      );
