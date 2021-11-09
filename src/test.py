a53c34c14bb78e67b3c5f3e0dafce31246cf8b82
iv8512 <82021077+iv8512@users.noreply.github.com>
2021-11-09 15:35:21 +0200

    steam game finder v0.1

---
+++ src/test.py
@@ -0,0 +1,13 @@
+import os, json
+
+path_start = os.path.expanduser("~")
+path_middle = r"AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam"
+path = os.path.join(path_start, path_middle)
+
+games = []
+
+for item in os.listdir(path):
+    games.append(item[:-4])
+
+with open("./data/steam_games.json", "w") as file:
+    file.write(json.dumps(games, indent=4))
