3dc26e1024abc07fc77f4fc063f097ef0f9119f9
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-11-10 18:29:46 +0200

    update part1/2

--- src/components/GamesSteam.js
+++ src/components/GamesSteam.js
@@ -2,22 +2,11 @@ function GamesSteam(props) {
     return ( 
         <div className="slide steam">
             <div className="header">
-                <h1>Steam Games</h1> <sub>SteamGameFinder {props.data.info.versions.file} - games installed: {props.data.steam.info.games}</sub>
+                <h1>Steam Games</h1> <sub>SteamGameFinder {props.data.info.versions.f} - games installed: {props.data.steam.info.games}</sub>
             </div>
 
 
             <div className="gameGrid">
-
-
-
-
-
-                <div className="gameCard">
-                    <a href={"steam://run/" + props.data.steam.games[21].id}>
-                        <img src={ "https://cdn.akamai.steamstatic.com/steam/apps/" + props.data.steam.games[21].id + "/capsule_184x69.jpg" } />
-                    </a>
-                </div>
-
                 {props.data.steam.games.map((data,index) => (
                     <div className="gameCard" key={index}>
                         <a href={"steam://run/" + data.id}>
@@ -25,11 +14,6 @@ function GamesSteam(props) {
                         </a>
                     </div>
                 ))}
-
-
-
-
-
             </div>
 
 
