e2335eacc4f90ffb6955830df08c3c4efd0e604e
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-12-01 22:57:36 +0200

    update

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -7,28 +7,33 @@ function Bar() {
 
 
         <div className="list listTop">
+        <Link to="/" className="navItem" text="Games">Games</Link>
+        <Link to="/ganttchart" className="navItem" text="Gantt Chart">Gantt Chart</Link>
+        <Link to="/test" className="navItem" text="Test Page">Test Page</Link>
 
-          <div className="dropDown">
+          {/* <div className="dropDown"> 
             Δ Pages Δ
             <div className="dropList">
               <Link to="/" className="navItem" text="Games"></Link>
               <Link to="/ganttchart" className="navItem" text="Gantt Chart"></Link>
               <Link to="/test" className="navItem" text="Test Page"></Link>
             </div>
-          </div>
+          </div> */}
 
         </div>
+
         <div className="logo">
           <img className="icon navItem" src="http://localhost:3000/favicon.ico" alt="set all to default" />
         </div>
-        <div className="list listBottom">
 
-          <div className="dropDown">
+        <div className="list listBottom">
+        <Link to="/" className="navItem" text="Settings (non-functional)">settings</Link>
+          {/* <div className="dropDown">
             Δ Other stuff Δ
             <div className="dropList">
               <Link to="/" className="navItem" text="Settings (non-functional)"></Link>
             </div>
-          </div>
+          </div> */}
 
         </div>
 
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,7 +3,7 @@ function Chart(props) {
     var widthMultiplier = 2
 
     return ( 
-        <div className="list">
+        <div className="chartList">
             <div className="row">
                 <span className="label">{props.data.rows[0].label}</span>
                 <span className="chartContent" style={{ marginLeft: props.data.rows[0].start * widthMultiplier + 'em', width: props.data.rows[0].end * widthMultiplier - props.data.rows[0].start * widthMultiplier + 'em', background: 'linear-gradient(90deg, var(--color-accent2) ' + props.data.rows[0].progress + '% , var(--color-dark1) ' + props.data.rows[0].progress + '%' }}></span>
