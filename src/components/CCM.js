8dfadd300334d1e98ffd455ed50c6d2065187884
JL6079 <82020564+JL6079@users.noreply.github.com>
2021-12-06 22:00:38 +0200

    Shortcut deletion fix

--- src/components/CCM.js
+++ src/components/CCM.js
@@ -1,84 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
 
 function CCM() {
 
+    
+    
     React.useEffect(() => {
-      
-        var taskItems = document.querySelectorAll(".task");
-      
-        for ( var i = 0, len = taskItems.length; i < len; i++ ) {
-          var taskItem = taskItems[i];
-          contextMenuListener(taskItem);
-        }
-      
-        function contextMenuListener(taskele) {
-            taskele.addEventListener( "contextmenu", function(e) {
-                e.preventDefault();
-                let CCM = document.getElementById('CCMContainer');
-                CCM.style.display = 'grid'
-
-                
-                setTimeout(function() {
-                    let launchcode = taskele.getAttribute('launch')
-                    let launchBTN = document.querySelector('.launchBTN');
-                    let delBTN = document.querySelector('.delBTN');
-                    launchBTN.setAttribute('href', launchcode);
-                    launchBTN.classList.add('active');
-                    console.log('launchURL', launchcode);
-                    
-                    if (taskele.classList.contains('shortcutGridItem')) {
-
-                        setTimeout(function() {
-                            var gridIndex = taskele.getAttribute('index');
-                            let storage = localStorage.getItem('links');
-                            storage = JSON.parse(storage);
-                            
-                            
-                            
-                            delBTN.classList.add('active');
-                            
-                            delBTN.addEventListener('click', function remover(){
-                                taskele.remove();
-                                console.log(taskele);
-                                
-                                storage.splice(gridIndex, 1);
-                                storage = JSON.stringify(storage);
-                                
-                                localStorage.setItem('links', storage)
-                            });
-                        }, 25);
-                    }
-                }, 25);
-            });
-        }
-        
-        document.addEventListener("contextmenu", function(){
-            let delBTN = document.querySelector('.delBTN');
-            let launchBTN = document.querySelector('.launchBTN');
-            delBTN.replaceWith(delBTN.cloneNode(true));
-
-            launchBTN.classList.remove('active');
-            launchBTN.setAttribute('href', window.location.href);
-
-            console.log('CCM reset');
-        })
-      
+        document.querySelector('.shortcuts').addEventListener('mousedown', function() {
+            var taskItems = document.querySelectorAll(".task");
+            
+            for ( var i = 0, len = taskItems.length; i < len; i++ ) {
+                var taskItem = taskItems[i];
+                contextMenuListener(taskItem);
+            }
+        });
     });
 
-    let [X, setX] = useState(0);
-    let [Y, setY] = useState(0);
+
+
+    function contextMenuListener(taskele) {
+        taskele.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+            let CCM = document.getElementById('CCMContainer');
+            CCM.style.display = 'grid'
+
+            
+            setTimeout(function() {
+                let launchcode = taskele.getAttribute('launch')
+                let launchBTN = document.querySelector('.launchBTN');
+                let delBTN = document.querySelector('.delBTN');
+                launchBTN.setAttribute('href', launchcode);
+                launchBTN.classList.add('active');
+                // console.log('launchURL', launchcode);
+                
+                if (taskele.classList.contains('shortcutGridItem')) {
+                    setTimeout(function() {
+                        var index = Array.from(taskele.parentNode.children).indexOf(taskele)
+
+                        let storage = localStorage.getItem('links');
+                        storage = JSON.parse(storage);
+                        
+                        
+                        
+                        delBTN.classList.add('active');
+                        
+                        delBTN.addEventListener('click', function remover(){
+                            taskele.remove();
+                            
+                            storage.splice(index, 1);
+                            storage = JSON.stringify(storage);
+                            
+                            localStorage.setItem('links', storage)
+                            console.log(storage);
+                        });
+                    }, 25);
+                }
+            }, 25);
+        });
+    }
+
+
+    document.addEventListener("contextmenu", function(){
+        let delBTN = document.querySelector('.delBTN');
+        let launchBTN = document.querySelector('.launchBTN');
+        delBTN.classList.remove('active');
+        delBTN.replaceWith(delBTN.cloneNode(true));
+
+        launchBTN.classList.remove('active');
+        launchBTN.setAttribute('href', window.location.href);
+    })
+
 
     React.useEffect(() => {
         const CCM = document.getElementById('CCMContainer');
 
-        let x;
-        let y;
-    
-        document.addEventListener('contextmenu', (data) => {
-            x = data.pageX + CCM.offsetWidth > document.innerWidth ? document.innerWidth - CCM.offsetWidth : data.pageX;
-            y = data.pageY + CCM.offsetHeight > document.innerHeight ? document.innerHeight - CCM.offsetHeight : data.pageY;
-            setX(x);
-            setY(y);
+        document.addEventListener('contextmenu', function(e) {
+            var x = e.pageX + CCM.offsetWidth > document.innerWidth ? document.innerWidth - CCM.offsetWidth : e.pageX;
+            var y = e.pageY + CCM.offsetHeight > document.innerHeight ? document.innerHeight - CCM.offsetHeight : e.pageY;
+            CCM.style.left = x + 'px';
+            CCM.style.top = y + 'px';
+            console.log(x, y);
         });
         
         document.addEventListener('mouseup', () => {
@@ -87,7 +87,7 @@ function CCM() {
     });
 
     return ( 
-        <div id="CCMContainer" style={{ top: Y, left: X }}>
+        <div id="CCMContainer">
             <div className="CCMItem"><a className="launchBTN clickable" target="_self" href={window.location.href}>launch</a></div>
             <div className="CCMItem"><div className="delBTN clickable">delete shortcut</div></div>
         </div>
