d19e7c6f8b69bd55f7f5b3e50d8892e87876d6d4
iv8512 <82021077+iv8512@users.noreply.github.com>
2021-12-08 01:57:08 +0200

    QStart Shortcut update

--- QStart Shortcut.py
+++ QStart Shortcut.py
@@ -1,4 +1,9 @@
 import os
+try:
+    import vdf, requests
+except ModuleNotFoundError:
+    os.system('cmd /c "pip install vdf"')
+    os.system('cmd /c "pip install requests"')
 
 os.system('cmd /c "cd src & cd python & jump_point.pyw"')
 
